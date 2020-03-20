06f0ed46932dbcebbf0d0b7ded826b8fed0fdf46
SimonVestman <simonw202@gmail.com>
2020-03-20 10:12:05 +0100

    tried to add some sort of a menu

--- src/NotepadEDITOR.java
+++ src/NotepadEDITOR.java
@@ -1,2 +1,46 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class NotepadEDITOR {
+    private JScrollBar scrollBar1;
+    private JTextArea textArea1;
+    private JButton Arkiv;
+    private JButton button2;
+    private JButton button3;
+    private JPanel panel;
+
+    static JMenuBar menuBar;
+    static JMenu menu;
+    static JMenuItem menuItem;
+
+
+
+    public NotepadEDITOR() {
+        Arkiv.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("NotepadEDITOR");
+        frame.setContentPane(new NotepadEDITOR().panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+        menuBar = new JMenuBar();
+        menu =  new JMenu("Stuff");
+        menuItem = new JMenuItem("HEj222");
+
+        menuBar.add(menu);
+        menu.add(menuItem);
+        frame.setJMenuBar(menuBar);
+
+    }
 }
+
+
